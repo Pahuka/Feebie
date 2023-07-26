da15ce8f3ad3785c6f06b7b1ba3d7dd5c165e555
Morf <Morf@DESKTOP-U435VF6>
2023-07-26 15:47:39 +0500

    Добавьте файлы проекта.

---
+++ ConsolePlotter/FreeSpaceChecker.cs
@@ -0,0 +1,34 @@
+﻿using System.Management;
+
+namespace ConsolePlotter;
+
+public class FreeSpaceChecker
+{
+	private long _freeSpace = 89120571392;
+
+	public bool Check(string deviceId)
+	{
+		var namespaceScope = new ManagementScope(@"\\.\ROOT\CIMV2");
+		var diskQuery = new ObjectQuery("SELECT * FROM Win32_MountPoint");
+		var mgmtObjSearcher = new ManagementObjectSearcher(namespaceScope, diskQuery);
+		var mountPoints = mgmtObjSearcher.Get();
+		var result = false;
+
+		foreach (var mp in mountPoints)
+		{
+			//Получаем доступ к объекту Win32_Directory (папка к которой примонтирован диск)
+			//var dirObj = new ManagementObject(mp.Properties["Directory"].Value as string);
+			//Получаем доступ к объекту Win32_Volume (раздел диска который примонтирован к папке)
+			var volumeObj = new ManagementObject(mp.Properties["Volume"].Value as string);
+
+			if (volumeObj.Path.Path.Contains(deviceId))
+			{
+				var currentSpace = long.Parse(volumeObj["FreeSpace"].ToString());
+				result = currentSpace >= _freeSpace;
+				break;
+			}
+		}
+
+		return result;
+	}
+}
---
+++ ConsolePlotter/Program.cs
@@ -0,0 +1,142 @@
+﻿using ConsolePlotter;
+
+var checker = new FreeSpaceChecker();
+
+Console.WriteLine("Run Plotter");
+
+var drivers = DriveInfo.GetDrives()
+	//.Where(x => x.DriveType == DriveType.Fixed)
+	.Where(x => x.DriveType != DriveType.CDRom)
+	.ToArray();
+
+Console.WriteLine("Укажите букву системного диска, который будет игнорироваться:");
+
+var systemDriver = Console.ReadLine().ToUpper() + ":\\";
+
+Console.WriteLine("Укажите букву диска, с которого будет перенос файлов:");
+
+var driverName = Console.ReadLine().ToUpper() + ":\\";
+
+var selectedDriver = drivers
+	.Where(x => x.Name == driverName)
+	.FirstOrDefault();
+
+if (selectedDriver == null)
+{
+	Console.WriteLine("Указанный диск не обнаружен");
+	Console.ReadLine();
+	return;
+}
+
+Console.WriteLine("Укажите путь к папке из которой будет производится перенос файлов:");
+
+var sourceDirectory = Path.Combine(selectedDriver.Name, Console.ReadLine());
+
+Console.WriteLine("Укажите путь к папке в которую будет производится перенос файлов:");
+
+var destDirectory = Path.Combine(Console.ReadLine());
+
+Console.WriteLine("Укажите время задержки в секундах для повторной проверки файла:");
+
+var pause = int.Parse(Console.ReadLine()) * 1000;
+
+var destinationDrivers = drivers
+	.Where(x => x.Name != driverName && x.Name != systemDriver)
+	.ToArray();
+
+Console.WriteLine($"Найдено {destinationDrivers.Length} дисков для копирования");
+
+while (true)
+{
+	var result = false;
+	Thread.Sleep(pause);
+
+	var file = Directory.GetFiles(sourceDirectory, "*.plot").FirstOrDefault();
+
+	if (file == null)
+	{
+		Console.WriteLine("Файла нет");
+		continue;
+	}
+
+	foreach (var driver in destinationDrivers)
+	{
+		if (driver.TotalFreeSpace >= 89120571392)
+		{
+			var newFilePath = Path.Combine(driver.Name, destDirectory, Path.GetFileName(file));
+
+			Console.WriteLine(
+				$"Файл есть, перемещаем в {newFilePath}");
+
+			Directory.CreateDirectory(Path.Combine(driver.Name, destDirectory));
+
+			var tempName = file + "Copy";
+			var tempPath = Path.Combine(driver.Name, destDirectory, Path.GetFileName(tempName));
+
+			using (var task = Task.Run(()=> { File.Move(file, tempName); }))
+			{
+				task.Wait();
+			}
+
+
+			using (var task = Task.Run(() => { File.Move(tempName, tempPath); }))
+			{
+				task.Wait();
+			}
+
+			using (var task = Task.Run(() => { File.Move(tempPath, newFilePath); }))
+			{
+				task.Wait();
+				result = task.IsCompletedSuccessfully;
+			}
+		}
+		else
+		{
+			var dirs = Directory.GetDirectories(driver.Name)
+				.Where(x => !x.Contains(destDirectory))
+				.Where(x => !String.IsNullOrEmpty(new DirectoryInfo(x).LinkTarget));
+
+			foreach (var dir in dirs)
+			{
+				if(!checker.Check(new DirectoryInfo(dir).LinkTarget))
+					continue;
+
+				var newPath = Path.Combine(driver.Name, dir, destDirectory, Path.GetFileName(file));
+
+				Console.WriteLine(
+					$"Файл есть, перемещаем в {newPath}");
+
+				Directory.CreateDirectory(Path.Combine(driver.Name, destDirectory));
+
+				var tempName = file + "Copy";
+				var tempPath = Path.Combine(driver.Name, dir, destDirectory, Path.GetFileName(tempName));
+
+				using (var task = Task.Run(() => { File.Move(file, tempName); }))
+				{
+					task.Wait();
+				}
+
+
+				using (var task = Task.Run(() => { File.Move(tempName, tempPath); }))
+				{
+					task.Wait();
+				}
+
+				using (var task = Task.Run(() => { File.Move(tempPath, newPath); }))
+				{
+					task.Wait();
+					result = task.IsCompletedSuccessfully;
+				}
+			}
+		}
+	}
+
+	if (!result)
+	{
+		Console.WriteLine("Файл не скопирован, не обнаружены диски со свободным объемом в 83 гигабайта");
+		break;
+	}
+}
+
+Console.WriteLine("Работа завершена");
+Console.ReadKey();
